cb8f6f89ffea2ca2602b6baec982776dbc0045a7
m-omar624 <muhammad.omar1141@gmail.com>
2023-02-09 15:15:20 -0500

    Muhammad Omar + Jacob Medeiros

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,36 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Book {
+    private String title;
+
+    private String author;
+
+    private String genre;
+    private String description;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public String getAuthor(){
+        return this.author;
+    }
+    public String getGenre(){
+        return this.genre;
+    }
+    public String getDescription(){
+        return this.description;
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,26 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Movie {
+    private String title;
+    private String overview;
+
+    public Movie(String title, String overview) {
+        this.title = title;
+        this.overview = overview;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public String getOverview(){
+        return this.overview;
+    }
 
 }
