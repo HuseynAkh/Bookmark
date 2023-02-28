c0d0fea21eb49a7d9a838880447ffdbb00e3d182
ethan <ethan.fifle@gmail.com>
2023-02-28 13:31:24 -0500

    Added Jar.exe

---
+++ src/home/BookmarkApplication.java
@@ -0,0 +1,26 @@
+package home;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BookmarkApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(BookmarkApplication.class.getResource("bookmark.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 1080);
+        stage.setTitle("Bookmark");
+        stage.setWidth(640);
+        stage.setHeight(840);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
