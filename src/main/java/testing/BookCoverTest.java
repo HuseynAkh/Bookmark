3f8a6242dc2997a33e340040a29af5ea76931373
m-omar624 <muhammad.omar1141@gmail.com>
2023-03-09 21:35:55 -0500

    Added book name search strategy test Added book cover url test

---
+++ src/main/java/testing/BookCoverTest.java
@@ -0,0 +1,25 @@
+package testing;
+
+import home.yorku.bookmarks.controller.search.CoverUrlExtractor;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookCoverTest {
+
+    @Test
+    void getSearchJSONBookCover() throws MalformedURLException {
+        String OLID = "OL22856696M";
+
+        CoverUrlExtractor cue = new CoverUrlExtractor();
+
+        String expectedJPGUrl = "https://covers.openlibrary.org/b/id/13257896-L.jpg";
+
+        cue.getBookCover(OLID);
+        String actualJPGUrl = cue.coverURL;
+
+        assertEquals(expectedJPGUrl,actualJPGUrl);
+    }
+}
