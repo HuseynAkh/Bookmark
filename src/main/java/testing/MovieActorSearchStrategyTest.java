ab56eafccd1de5df6f5626247647fede3961e2aa
Jacob <jacobm0813@gmail.com>
2023-03-09 20:51:43 -0500

    Added JUnit testing classes for MovieTitleSearch and MovieActorSearch

---
+++ src/main/java/testing/MovieActorSearchStrategyTest.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieActorSearchStrategyTest {
+
+    @Test
+    void getSearchURL() {
+    }
+}
